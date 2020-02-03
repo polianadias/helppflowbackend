3cc9d2ae8c9202c381cd97c4b4e012f17a14df83
Poliana Dias <polianadias@outlook.com>
2020-02-03 18:17:02 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+
+const app = express()
+mongoose.connect('mongodb://localhost:27017/upload')
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(morgan('dev'))
+app.use(require("./routes"))
+
+app.listen(3001)
