1f9e5873334f161ab0315dbf48a9befb3120916c
Poliana Dias <polianadias@outlook.com>
2020-02-11 13:35:27 -0300

    adding configuration aws s3

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,10 +1,12 @@
 const multer = require('multer')
 const path = require('path')
 const crypto = require('crypto')
+const multerS3 = require('multer-s3')
+const aws = require('aws-sdk')
 
-module.exports = {
-    dest: path.resolve(__dirname, '..', '..', 'tmp', 'uploads'),
-    storage: multer.diskStorage({
+
+const storageTypes = {
+    local: multer.diskStorage({
         destination: (req, file, cb) => {
             cb(null, path.resolve(__dirname, '..', '..', 'tmp', 'uploads'))
         },
@@ -12,20 +14,39 @@ module.exports = {
             crypto.randomBytes(16, (err, hash) => {
                 if (err) cb(err)
 
-                const fileName = `${hash.toString('hex')}-${file.originalname}`
-                cb(null, fileName);
+                file.key = `${hash.toString('hex')}-${file.originalname}`
+                cb(null, file.key);
             })
         }
     }),
+
+    s3: multerS3({
+        s3: new aws.S3(),
+        bucket: 'helppflow',
+        contentType: multerS3.AUTO_CONTENT_TYPE,
+        acl: 'public-read',
+        key: (req, file, cb) => {
+            crypto.randomBytes(16, (err, hash) => {
+                if (err) cb(err)
+
+                const fileName = `${hash.toString('hex')}-${file.originalname}`
+                cb(null, fileName);
+            })
+
+        }
+    })
+}
+
+
+
+module.exports = {
+    dest: path.resolve(__dirname, '..', '..', 'tmp', 'uploads'),
+    storage: storageTypes["local"],
     limits: {
         filesize: 2 * 1024 * 1024
     },
     fileFilter: (req, file, cb) => {
         const allowedMimes = [
-            "image/jpeg",
-            "image/pjpeg",
-            "image/png",
-            "image/gif",
             "application/xml",
             "text/xml"
         ]
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,22 @@
 const routes = require("express").Router()
 const multer = require("multer");
 const multerConfig = require("./config/multer");
+const Post = require('./models/Post')
 
 
-routes.post('/posts', multer(multerConfig).single("file"), (req, res) => {
+routes.post('/posts', multer(multerConfig).single("file"), async (req, res) => {
 
-    console.log(req.file)
-    return res.json({ hello: "World teste" })
+    const { originalName: name, size, key } = req.file
+
+    const post = await Post.create({
+        name,
+        size,
+        key,
+        url: ""
+
+    })
+
+    return res.json(post)
 })
 
 module.exports = routes;
