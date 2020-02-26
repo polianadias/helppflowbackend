7ed192381b9a13a71a0793fe5d51ca8b5eb753f4
Poliana Dias <polianadias@outlook.com>
2020-02-26 15:30:53 -0300

    add configration with aws s3

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,20 +1,21 @@
-const multer = require('multer')
-const path = require('path')
-const crypto = require('crypto')
-const multerS3 = require('multer-s3')
-const aws = require('aws-sdk')
+const multer = require("multer")
+const path = require("path")
+const crypto = require("crypto")
+const aws = require("aws-sdk");
+const multerS3 = require("multer-s3");
 
 
 const storageTypes = {
     local: multer.diskStorage({
         destination: (req, file, cb) => {
-            cb(null, path.resolve(__dirname, '..', '..', 'tmp', 'uploads'))
+            cb(null, path.resolve(__dirname, "..", "..", "tmp", "uploads"))
         },
         filename: (req, file, cb) => {
             crypto.randomBytes(16, (err, hash) => {
                 if (err) cb(err)
 
-                file.key = `${hash.toString('hex')}-${file.originalname}`
+                file.key = `${hash.toString("hex")}-${file.originalname}`;
+                
                 cb(null, file.key);
             })
         }
@@ -22,26 +23,26 @@ const storageTypes = {
 
     s3: multerS3({
         s3: new aws.S3(),
-        bucket: 'helppflow',
+        bucket: process.env.BUCKET_NAME,
         contentType: multerS3.AUTO_CONTENT_TYPE,
-        acl: 'public-read',
+        acl: "public-read",
         key: (req, file, cb) => {
             crypto.randomBytes(16, (err, hash) => {
                 if (err) cb(err)
 
-                const fileName = `${hash.toString('hex')}-${file.originalname}`
+                const fileName = `${hash.toString("hex")}-${file.originalname}`
                 cb(null, fileName);
+
+                
             })
 
         }
     })
 }
 
-
-
 module.exports = {
-    dest: path.resolve(__dirname, '..', '..', 'tmp', 'uploads'),
-    storage: storageTypes["local"],
+    dest: path.resolve(__dirname, "..", "..", "tmp", "uploads"),
+    storage: storageTypes[process.env.STORAGE_TYPE],
     limits: {
         filesize: 2 * 1024 * 1024
     },
@@ -50,7 +51,7 @@ module.exports = {
             "application/xml",
             "text/xml"
         ]
-        console.log('allow type: ', req)
+
         if (allowedMimes.includes(file.mimetype)) {
             cb(null, true)
         } else {
--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,38 @@
 const routes = require("express").Router()
 const multer = require("multer");
 const multerConfig = require("./config/multer");
+
+
 const Post = require('./models/Post')
+
+routes.get('/posts', async (req, res) => {
+    const post = await Post.find()
+    return res.json(post)
+})
+
 
 
 routes.post('/posts', multer(multerConfig).single("file"), async (req, res) => {
 
-    const { originalName: name, size, key } = req.file
-
+    const { originalName: name, size, key, location: url = "" } = req.file
+    console.log(req.file)
     const post = await Post.create({
         name,
         size,
         key,
-        url: ""
+        url
 
     })
-
+    
     return res.json(post)
 })
+
+routes.delete("/posts/:id", async (req, res) => {
+    const post = await Post.findById(req.params.id);
+  
+    await post.remove();
+  
+    return res.send();
+  });
 
 module.exports = routes;
